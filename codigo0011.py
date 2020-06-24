8805dc1fb3f67f0b04764bdc508ee39bb3893a3e
Ricardo Rios <mathricardorios@gmail.com>
2020-06-24 11:37:03 -0600

    Add python files

---
+++ codigo0011.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+s = pd.Series(['dog', 2])
+print(s)
+
+s = pd.Series(['Wes MKinney', 'Creator of pandas'], index=['Person','Who'])
+print(s)
---
+++ codigo0012.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+smartdudes = pd.DataFrame( \
+               data={'Occupation':['Genius', 'SmartestHumanEver'],
+               'Born':['1879-03-14', '1643-01-04'],
+               'Died':['1955-04-18', '1727-03-31'],
+               'Age':[76, 84]},
+               index=['Albert Einstein', 'Isaac Newton'],
+               columns=['Occupation','Born','Died','Age'])
+               
+
+print(smartdudes)
+
+first_row = smartdudes.loc['Albert Einstein']
+print(type(first_row))
+print(first_row)
+
